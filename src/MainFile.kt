85ea8fa0508657f9deb006d9f79f8553650a1305
arccassin <arccassin@gmail.com>
2019-09-29 18:23:58 +0300

    Initial commit

---
+++ src/MainFile.kt
@@ -0,0 +1,14 @@
+/**
+ * Created by User on 29 Сент., 2019
+ * Распечатать и написать в качестве ответа к домашнему заданию максимальные и минимальные значения всех типов чисел.
+
+ */
+
+fun main(args: Array<String>) {
+    print("Byte: min = ${Byte.MIN_VALUE}, max = ${Byte.MAX_VALUE}\n");
+    print("Short: min = ${Short.MIN_VALUE}, max = ${Short.MAX_VALUE}\n");
+    print("Int: min = ${Int.MIN_VALUE}, max = ${Int.MAX_VALUE}\n");
+    print("Long: min = ${Long.MIN_VALUE}, max = ${Long.MAX_VALUE}\n");
+    print("Float: min = ${Float.MIN_VALUE}, max = ${Float.MAX_VALUE}\n");
+    print("Double: min = ${Double.MIN_VALUE}, max = ${Double.MAX_VALUE}\n");
+}
